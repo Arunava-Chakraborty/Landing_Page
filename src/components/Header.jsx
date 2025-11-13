1519a739e9ba362d5eaf8639c3d751339617bcac
Arunava Chakraborty <oarunavachakraborty@gmail.com>
2025-11-13 20:45:11 +0530

    updating logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,15 +36,9 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img
-            src={brainwave}
-            width={190}
-            height={10}
-            alt="Brainwave"
-            className="pointer-events-none select-none"
-          />
-        </a>
+       <a className="block w-[12rem] xl:mr-8" href="#hero">
+            <img src="/assets/benefits/image-2.png" alt="enlift_logo" />
+          </a>
 
         <nav
           className={`${
--- src/components/design/Header.jsx
+++ src/components/design/Header.jsx
@@ -32,13 +32,9 @@ export const HambugerMenu = () => {
   return (
     <div className="absolute inset-0 pointer-events-none lg:hidden">
       <div className="absolute inset-0 opacity-[.03]">
-        <img
-          className="w-full h-full object-cover"
-          src={background}
-          width={688}
-          height={30}
-          alt="Background"
-        />
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+              < img src="/assets/benefits/image-2.png" alt="enlift_logo" />
+        </a>
       </div>
 
       <Rings />
