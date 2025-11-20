2ff389cb9783a36d69aadd1049d2ba5e10acee4a
DataHashArunava <118994880+Arunava-Chakraborty@users.noreply.github.com>
2025-11-20 08:36:59 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
 export const links = {
   sourceCode: "https://enlift-ai.vercel.app/hashtag",
+  i_Studio: "https://enlift-ai.vercel.app/dashboard",
+  roadmap:"https://enlift-ai.vercel.app/roadmap"
 };
